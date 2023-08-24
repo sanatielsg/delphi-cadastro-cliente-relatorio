d0f8f7c91a8f9f7738aeb777c06bb8b7cea92a09
Sanatiel <sanatielsg@gmail.com>
2023-08-24 15:26:53 -0300

    Criando abas de cadastro e consulta

---
+++ Cliente.dpr
@@ -0,0 +1,14 @@
+program Cliente;
+
+uses
+  Vcl.Forms,
+  u_main in 'u_main.pas' {FrmMain};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TFrmMain, FrmMain);
+  Application.Run;
+end.
---
+++ u_main.pas
@@ -0,0 +1,27 @@
+unit u_main;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ComCtrls;
+
+type
+  TFrmMain = class(TForm)
+    PgcMain: TPageControl;
+    TSCadastro: TTabSheet;
+    TSConsulta: TTabSheet;
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  FrmMain: TFrmMain;
+
+implementation
+
+{$R *.dfm}
+
+end.
